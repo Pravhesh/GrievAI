bdfad6a1943ba1b524ec10f1408e1159429fc0d2
Pravhesh V <praavesh.v@gmail.com>
2025-07-11 20:09:11 +0530

    Switch network from Goerli to Sepolia

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
-const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "";
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.20",
   networks: {
-    goerli: {
-      url: GOERLI_RPC_URL,
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
       accounts: PRIVATE_KEY !== "0x" ? [PRIVATE_KEY] : [],
     },
   },
